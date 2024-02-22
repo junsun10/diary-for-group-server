7a53a6f720611f5c7de695ead1e03c2c3141c5d2
Junsun Lee <junsun1014@naver.com>
2024-02-23 00:58:38 +0900

    feat: add find method

--- src/main/java/com/example/diary/repository/GroupMemberRepository.java
+++ src/main/java/com/example/diary/repository/GroupMemberRepository.java
@@ -5,10 +5,12 @@ import com.example.diary.domain.group.GroupMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> {
 
     void deleteByGroupIdAndMemberId(Long groupId, Long memberId);
 
     List<GroupMember> findByMemberId(Long memberId);
+    Optional<GroupMember> findByMemberIdAndGroupId(Long memberId, Long groupId);
 }
