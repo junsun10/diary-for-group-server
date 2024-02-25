b4cfea379c490cbf110b94a611c93df0e899d917
Junsun Lee <junsun1014@naver.com>
2024-02-25 22:09:30 +0900

    feat: add exclude path pattern and handle preflight request

--- src/main/java/com/example/diary/config/WebConfig.java
+++ src/main/java/com/example/diary/config/WebConfig.java
@@ -15,16 +15,18 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LoginCheckInterceptor())
                 .order(1)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/", "/members/new", "/members/login", "/members/logout",
+                .excludePathPatterns("/", "/members/new", "/members/login", "/members/logout", "/members/login-status",
                         "/css/**", "/*.ico", "/error");
     }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 허용할 경로 설정
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 메소드 설정
                 .allowedOrigins("http://localhost:8081") // 허용할 오리진 설정
-                .allowedMethods("*") // 허용할 메소드 설정
+                .allowedHeaders("*") // 허용할 헤더 설정
                 .allowCredentials(true) // 인증 정보 허용
-                .allowedHeaders("*"); // 허용할 헤더 설정
+                .maxAge(1800); // preflight 요청 캐시 시간(초)
     }
 }
+
--- src/main/java/com/example/diary/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/example/diary/interceptor/LoginCheckInterceptor.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.thymeleaf.util.StringUtils;
 
 @Slf4j
 public class LoginCheckInterceptor implements HandlerInterceptor {
@@ -16,15 +17,26 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
 
         String requestURI = request.getRequestURI();
 
+        // preflight
+        if (request.getMethod().equals("OPTIONS")) {
+            return true;
+        }
+
         log.info("로그인 체크 인터셉터 실행 {}", requestURI);
 
-        HttpSession session = request.getSession();
+        HttpSession session = request.getSession(false);
 
         if (session == null || session.getAttribute(SessionConst.LOGIN_MEMBER_ID) == null) {
-
+            if (session == null) {
+                log.info("세션이 없습니다.");
+            }
+            else {
+                log.info("로그인 정보가 없습니다.");
+            }
             log.info("미인증 사용자 요청 {}", requestURI);
             throw new AuthenticationException("로그인이 필요합니다.");
         }
+
         return true;
     }
 }
